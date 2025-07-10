8e83f1950ab57fc726473045935c68bb24db4947
Ayush Bajracharya <ayush@NVT-L-099s-MacBook-Pro.local>
2025-07-10 23:11:23 +0545

    feat: add a header to design rsh

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,15 @@ import {
 export default function IndexPage() {
   return (
     <>
+      <div className="fixed-header font-medium">
+        ✨&nbsp;My work on&nbsp;<a href="https://sports.kayacorp.com.np/" target="_blank" rel="noopener noreferrer" className='font-bold underline'>Kaya Sports</a>&nbsp;got featured on&nbsp;<a href="https://www.designrush.com/best-designs/websites" target="_blank" rel="noopener noreferrer" className='font-bold underline'>Design Rush</a>&nbsp;✨
+      </div>
       <Head>
         <title>Ayush Bajra | Software Developer</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className='container'>
+      <div className='container' style={{ paddingTop: '20px' }}>
         <Intro />
         <div className='h-6 md:h-24'></div>
         <Background />
